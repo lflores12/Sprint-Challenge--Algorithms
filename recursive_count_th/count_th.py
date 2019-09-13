74071dad050894ba0928468b5a46f4250376f4cf
Leonel <leonelaf12@gmail.com>
2019-09-13 11:26:28 -0400

    finished with count_th first to get the challenges out of the way first

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,6 +5,16 @@ Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
     
-    # TBC
+    #need a base case. if i am checking every single every in order from the first one on to the letter next to it, i need to stop at the last letter.
+    if not word:
+        return 0
+    #check if letter [0] and [1] are 'th'
+    if word[0:2] == 'th':
+        #increment count and continue on
+        return 1 + count_th(word[1:])
+    #if not continue with the next letter
+    else:
+        return count_th(word[1:])
+        
     
-    pass
+    
